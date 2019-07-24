b686bf82888a4c7c5dbcad2199e4fdf38164df48
CFonseca11 <fonseca.carmen2000@gmail.com>
2019-07-24 12:20:05 +0100

    Função menu()

--- Interação/Arduino_Uno/Arduino_Uno.ino
+++ Interação/Arduino_Uno/Arduino_Uno.ino
@@ -2,7 +2,7 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define NUM_MOV 4 //numero de movimentos conhecidos
+#define NUM_MOV 5 //numero de movimentos conhecidos
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
@@ -13,13 +13,46 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();          //Leitura cartão
   mfrc522.PCD_Init();
-  Serial.println("Put your card to the reader...");
-  Serial.println();
 }
 
 String carmen_card = "94 92 AE CB";
 String joao_card = "15 55 89 92";
 byte movimento;
+String player_name = "";
+String option = "";
+
+void menu() {
+  Serial.println("----- Welcome to the Bionic Arm! -----\n What's your name?");
+
+  while (!Serial.available() && player_name == "") {
+    player_name = Serial.readString();
+  }
+  Serial.print("Hello, ");
+  Serial.print(player_name);
+
+  Serial.println("What would you like to do?\n1- Play Rock, Paper, Scissors\n2- Read my school card for a special move");
+
+  while (!Serial.available() && (option.charAt(0) != '1' || option.charAt(0) != '2') && option == "") {
+    option = Serial.readString();
+    if (option.charAt(0) != '1' && option.charAt(0) != '2' && option != "") {
+      Serial.println("Sorry, that's not a valid option! Try again.");
+      option = "";
+    }
+  }
+
+  if (option.charAt(0) == '1') {
+    game();
+  }
+  else if (option.charAt(0) == '2') {
+    read_card();
+  }
+  option = "";
+  player_name = "";
+}
+
+void game() {
+  Serial.println("Game");
+}
 
 bool get_card() {
   if ( ! mfrc522.PICC_IsNewCardPresent()) { // Look for new cards
@@ -32,16 +65,9 @@ bool get_card() {
   return true;
 }
 
-void seleciona_movimento(byte num) {
-  Wire.beginTransmission(8);
-  Wire.write(num);
-  Wire.endTransmission();
-}
-
-void loop() {
-  movimento = 0;
-
-  if (get_card()) {
+void read_card() {
+  Serial.println("Put your card to the reader, please...");
+  while (!get_card()) {
     Serial.print("UID tag :");
     String content = "";
     for (byte i = 0; i < mfrc522.uid.size; i++) {
@@ -61,16 +87,17 @@ void loop() {
       Serial.println();
       delay(500);
     }
-
-
+    
     Serial.println(" Access denied");
   }
+}
 
-  while (movimento < NUM_MOV) {
-    seleciona_movimento(movimento);
-    Serial.println(movimento);
-    movimento ++;
-    delay(1000);
-  }
-  delay(1000);
+void seleciona_movimento(byte num) {
+  Wire.beginTransmission(8);
+  Wire.write(num);
+  Wire.endTransmission();
+}
+
+void loop() {
+  menu();
 }
